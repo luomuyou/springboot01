cef3116b62bd1a68a1b1d638c870fb3ef6ca7719
luoquan <luomuyou@foxmail.com>
2022-09-13 05:31:58 +0800

    分支222

--- src/main/java/com/lmy/config/JWTConfig.java
+++ src/main/java/com/lmy/config/JWTConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 //@Configuration
 public class JWTConfig implements WebMvcConfigurer {
     @Override
-    public void addInterceptors(InterceptorRegistry registry) { //拦截器注入111112
+    public void addInterceptors(InterceptorRegistry registry) { //拦截器注入1111123
         registry.addInterceptor(new JWTInterceptor())   //注入自定义拦截器类
                 .excludePathPatterns("/JWT/JWTlogin") //登录请求之外
                 .addPathPatterns("/JWT/JWTtest");     //拦截所有请求
