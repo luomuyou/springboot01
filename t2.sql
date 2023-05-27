db3d6df6f9948e8fb9e9d1b6b2bcf33e36eeec0a
luoquan <luomuyou@foxmail.com>
2023-05-27 18:10:50 +0800

    回滚操作1.0

--- t2.sql
+++ t2.sql
@@ -23,7 +23,10 @@ test01 sql 04
 
 测试111
 
-commit未push
+回滚本地的：
+    文件右键->Git->rollback->回滚到未commit状态
+
+commit未push(回滚本地缓存区的)：
     undo commit ： undo commit 只能回滚最后一条commit记录
     drop commit ：直接找不到
     revert commit:到之前的某一次提交记录
@@ -33,3 +36,5 @@ commit未push
         mixed ：如果有新增add的新文件，会便红色未被changes监控状态，我其他跟soft模式差不多，
         hard：本地代码也找不到了，没有记录，新增文件还在
         keep：本地代码也找不到了，没有记录，新增文件也找不见了
+
+已经push了(回滚远程的):
