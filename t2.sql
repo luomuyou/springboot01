6555861999663b84eeac148dfe9ca001510d2cf6
luoquan <luomuyou@foxmail.com>
2023-05-27 18:07:07 +0800

    commit未push

--- t2.sql
+++ t2.sql
@@ -22,3 +22,14 @@ test01 sql 04
 6666
 
 测试111
+
+commit未push
+    undo commit ： undo commit 只能回滚最后一条commit记录
+    drop commit ：直接找不到
+    revert commit:到之前的某一次提交记录
+
+    Reset Current Branch to Here：有几种模式可选
+        soft  ：回退，本地到代码也存在，到未commit状态
+        mixed ：如果有新增add的新文件，会便红色未被changes监控状态，我其他跟soft模式差不多，
+        hard：本地代码也找不到了，没有记录，新增文件还在
+        keep：本地代码也找不到了，没有记录，新增文件也找不见了
